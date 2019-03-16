24c0869084599efb3b66ac5709f0a7bd183360c4
Md. Momin Al Aziz <azizmma@compsci.cs.umanitoba.ca>
2019-03-16 18:25:26 -0500

    ITS anon first commit

---
+++ README.md.txt
@@ -0,0 +1,20 @@
+# Its anonymizer v.2
+This version of the its anonymizer processes files with a simple text editor, which allows to preserve original structure of the its file. It takes a path of the folder that contains the original its files (input its) and creates a copy of that folder with anonymized files  named 'inputfolder_itsFolder' on the same file location.
+
+##To use
+
+1. Open the command window and navigate to the program folder. Or open the program folder, Shift+right-click on empty space, 'Open command window here'
+2. Copy path of the folder with the its files (change drive if necessary, i.e., `S:`)
+3. In the command winodw type `python its_anonymizer.py` and paste the path which contains the input its files. Command should look like this:
+`python its_anonymizer.py "C:\input_its_folder`
+
+The program will create a new folder in the same directory as the input folder adding suffix "_anonymized". If it exists, it will NOT override; throw an exception. 
+
+##Changes
+
+Dates:		1000-01-01 00:00:00 (January 1, 1000, 12 AM)
+Time zone: 	AAA
+Gender: 		A
+Child key: 	9A99AAA999999A99
+
+This information is stored in `repacement_dict.json` will dictate what the anonymized file will have. This file can be modified to include other information that needs to be private.
---
+++ its_anonymizer.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import re
+import time
+import json
+import glob
+import sys
+import os
+
+def load_replacements_file(filePath): #'replacement_dict.json'
+    with open(filePath) as f:
+        replacements = json.load(f)
+    return replacements
+
+def parse_file(inFile, outFile, replacements):
+
+    with open(inFile, 'r') as inF:
+        with open(outFile, 'w') as outF:
+        
+            # print('\n' + os.path.split(inFile)[1] + ':')
+            
+            for line in inF:
+                for node in replacements.keys():
+                    # print(line)
+                    if re.search(r'<{}\b'.format(node), line): # word boundary is important here
+                        for name, value in replacements[node].items():
+                            line = re.sub(r'{}="[a-zA-Z0-9_.:\-]*"'.format(name),
+                                          r'{}="{}"'.format(name, value),
+                                          line)
+                            # print('\t- changed {}/{} to {}'.format(node, name, value))
+                outF.write(line)
+
+
+if __name__ == '__main__':
+    itsFolder = sys.argv[1]
+
+    # Check that the ist files directory exists
+    if not os.path.isdir(itsFolder):
+        raise Exception('Not a valid folder name')
+
+    # Get paths of the its files
+    files = glob.glob(os.path.join(itsFolder, '*.its'))
+    if not files:
+        raise Exception('There are no its files in the folder')
+
+    # Make the output folder
+    outFolder = itsFolder + '_anonymised'
+    if os.path.isdir(outFolder):
+        raise Exception('The output folder already exists, please consider delete/renaming it.')
+    else:
+        os.mkdir(outFolder)
+
+    # Load the replacements dictionary
+    replacements = load_replacements_file('./replacements_dict.json')
+
+    # Process all files
+    for inFile in files:
+
+        _, name = os.path.split(inFile)
+        outFile = os.path.join(outFolder, name)
+
+        parse_file(inFile, outFile, replacements)
+        print('done {}'.format(name))
+    
+    print('\n-----\nProcessed {} files'.format(len(files)))
