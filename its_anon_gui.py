5e94a62012e8d46cf0a99fa4331960f2b37bb6b5
Sarah MacEwan <smacewan@mts.net>
2019-07-09 16:41:21 -0500

    Add file IO and checkbuttons
    Added functionality to the directory choosers, and also added buttons for io and for what to anonymize to the gui

--- its_anon_gui.py
+++ its_anon_gui.py
@@ -23,7 +23,7 @@ class Anonymizer(object):
     
     def __init__(self, master):
         self.root = master
-        self.root.resizable(width=False, height=False)
+        self.root.resizable(width=True, height=True)
         self.root.title('ITS Anonymizer')
         self.root.protocol('WM_DELETE_WINDOW')# add function to register pressing the x button as event and call the corresponding function
 
@@ -40,32 +40,83 @@ class Anonymizer(object):
         readme_link = 'https://github.com/BLLManitoba/ITS_anonymizer/blob/master/README.md'
         self.submenu.add_command(label='Help', command=lambda: webbrowser.open_new(readme_link))
 
-        w = tk.Label(root, text = "Hello, world!").grid(row = 0, column = 0)
+        # Input/Output Selection Buttons
+        self.input_dir_button = tk.Button(
+            self.frame,
+            text = 'Select input files', 
+            command = self.select_input_its,
+            height = 1,
+            width = 20,
+            relief=tk.GROOVE).grid(row=0, column=0, padx=20, pady=5)
+
+        self.output_dir_button = tk.Button(
+            self.frame,
+            text = 'Select output location',
+            command = self.select_output_dir,
+            height = 1,
+            width = 20,
+            relief = tk.GROOVE).grid(row = 1, column = 0, padx=20, pady=5)
+            
+        # Type of info to anonymize checkbuttons
+        self.primaryChild_checkbox = tk.Checkbutton(
+            self.frame,
+            text = 'Primary Child Data',
+            onvalue=True,
+            offvalue=False).grid(row=0, column = 1, padx=5, pady=5, sticky='NW')
+        self.its_checkbox = tk.Checkbutton(
+            self.frame,
+            text = 'ITS file data',
+            onvalue=True,
+            offvalue=False).grid(row=1, column = 1, padx=5, pady=5, sticky='NW')
+        self.transfer_checkbox = tk.Checkbutton(
+            self.frame,
+            text = 'File transfer data',
+            onvalue=True,
+            offvalue=False).grid(row=2, column = 1, padx=5, pady=5, sticky='NW')
+        self.childInfo_checkbox = tk.Checkbutton(
+            self.frame,
+            text = 'Child birthdate and gender',
+            onvalue=True,
+            offvalue=False).grid(row=3, column = 1, padx=5, pady=5, sticky='NW')
+        self.clocktime_checkbox = tk.Checkbutton(
+            self.frame,
+            text = 'Clock start/stop',
+            onvalue=True,
+            offvalue=False).grid(row=4, column = 1, padx=5, pady=5, sticky='NW')
+            
+            
         # TODO:
             # Add buttons to select directory of its files
             # Add a set of checkboxes to list off the things that they want anonymized
             # Add a "save anonymized files as..." button so they can save the files in a specified location
             # Add a big old "anonymize" button :D
             
-            
     def select_input_its(self):
+        print('selecting inputs...')
+        input_dir = tkFileDialog.askdirectory()
+        return input_dir
         # TODO: pull up a filechooser to select the directory of its files you want anonymized
         #set that directory as your input directory
         
     def select_output_dir(self):
+        print('selecting output dir...')
+        output_dir = tkFileDialog.askdirectory(title='Select where to save your output files')
+        return output_dir
         # TODO: pull up a filechooser to select where you want to save your anon'd files
         # let them name the directory and then set that as the output dir
-
+        
     def anonymize_its_files_full(self):
         print("anonymizing your its files the old fashioned way :P")
         # TDOD: add functionality to this function -> call an anonymizer object to anonymize the files???
         #self.its_anonymizer.main()
         
-    def select_items_anonymize(self):
+    def select_type_anonymize(self):
+        print('select the kinds of things you want to anonymize')
         #TODO: add funcionality to select the certain data to anonymize
         # returns a list of things to anonymize
         
     def anonymize_its_files(self, anon_list):
+        print('anonymizing files...')
         #TODO: this function will take into account the list of things to anonymize and only blank out that stuff!
 
 
