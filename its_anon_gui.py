099c21b2cbed29242467c06f2074373962b0c0a1
Sarah MacEwan <smacewan@mts.net>
2019-07-09 14:53:35 -0500

    Create its_anon_gui.py
    Created a python file that will eventually serve as a gui for the main its_anonymizer.py script

---
+++ its_anon_gui.py
@@ -0,0 +1,10 @@
+# GUI builder for .its anonymizer app
+# Created by: Sarah MacEwan
+# Last Updated: July 9, 2019
+
+import Tkinter as tk
+
+root = tk.Tk()
+w = tk.Label(root, text = "Hello, world!").grid(row = 0, column = 0)
+
+root.mainloop()
